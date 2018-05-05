b0a5cb0080cc647241cde9e150a6d93d3cc867e7
F1L1N <pavel9696@yandex.ru>
2018-05-05 10:20:50 +0300

    Initial commit

---
+++ src/Paint.java
@@ -0,0 +1,66 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Paint {
+    //пути с файлами данных, использованных для построения графиков
+    private static String Filename_graph_x = System.getProperty("user.dir")+"\\data\\x.txt";
+    private static String Filename_graph_y = System.getProperty("user.dir")+"\\data\\y.txt";
+    private static String Filename_graph = System.getProperty("user.dir")+"\\data\\graph.jpeg";
+
+    //методы для вывода данных из коллекции XYSeries
+    private static void show (XYSeries series, int t0, int T){
+        for (int i = t0; i < T; i++){
+            System.out.println("x = "+series.getX(i)+", y = "+series.getY(i));
+        }
+    }
+
+    private static void show (XYSeries series){
+        for (int i = 0; i < series.getItemCount(); i++){
+            System.out.println("DataItem["+i+"] = "+series.getDataItem(i));
+        }
+    }
+
+    //метод для формирования XYSeries - коллекции
+    private static XYSeries read_file() throws IOException {
+        XYSeries series = new XYSeries("σ(t)");
+        BufferedReader reader_x = new BufferedReader(new FileReader(Filename_graph_x));
+        BufferedReader reader_y = new BufferedReader(new FileReader(Filename_graph_y));
+        String lineX, lineY;
+        while (((lineX = reader_x.readLine()) != null)&&(lineY = reader_y.readLine()) != null) {
+            series.add(Double.parseDouble(lineX), Double.parseDouble(lineY));
+        }
+        return series;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int width = 1280;
+        int height = 720;
+        //формирование XYSeries - коллекции
+        XYSeries series = read_file();
+        //формирование набора данных на основе ранее созданной коллекции
+        XYDataset xyDataset = new XYSeriesCollection(series);
+        JFreeChart chart = ChartFactory
+                .createXYLineChart("y = σ(t)", "t", "σ",
+                        xyDataset,
+                        PlotOrientation.VERTICAL,
+                        true, true, true);
+        JFrame frame = new JFrame("MinimalStaticChart");
+        // Помещаем график на фрейм
+        frame.getContentPane().add(new ChartPanel(chart));
+        frame.setSize(width,height);
+        ChartUtilities.saveChartAsJPEG( new File(Filename_graph), chart , width , height );
+        frame.show();
+    }
+}
